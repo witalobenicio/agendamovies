0e688eb7606673cea8c85588732a24cf1a3b05d4
Witalo Benício <contato@witalobenicio.com>
2018-11-06 22:36:08 -0300

    configuring project with webpack, eslint, babel and material-ui

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+  state = {};
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <p>React Challenge E-masters</p>
+        </header>
+      </div>
+    );
+  }
+}
+
+export default App;
