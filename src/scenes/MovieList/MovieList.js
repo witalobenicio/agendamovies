878658ed3095727d133b61e4ebe0ecd1d4bb1ffe
Witalo Benício <contato@witalobenicio.com>
2018-11-25 13:25:12 -0300

    new(movielist): adding progress color

--- src/scenes/MovieList/MovieList.js
+++ src/scenes/MovieList/MovieList.js
@@ -2,44 +2,47 @@
 
 import React from 'react';
 import { MovieItem, RecyclerList } from '~/components';
+import style from './MovieList.less';
 import CircularProgress from '@material-ui/core/CircularProgress';
 
 type Props = {
   items: [],
+  totalItems: number,
   onPressFavorite: () => void,
   onPressMovie: () => void,
   onEndReached: () => void,
 }
 
-const STATUS_LOADING = 1;
-const STATUS_LOADED = 2;
-
 class MovieList extends React.Component<Props, void> {
-  state = {
-    page: 0,
-  };
-
   loadMoreRows = () => {
     this.props.onEndReached();
   };
 
   render() {
-    const { items } = this.props;
+    const { items, totalItems = 20000 } = this.props;
     return (
-      <RecyclerList
-        onLoadMore={this.loadMoreRows}
-        class="list"
-        overscan={6}
-      >
-        { items.map(item => (
-          <MovieItem
-            onPressMovie={this.props.onPressMovie}
-            onPressFavorite={this.props.onPressFavorite}
-            item={item}
-          />
-        )) }
-        <CircularProgress />
-      </RecyclerList>
+      <div>
+        <RecyclerList
+          onLoadMore={this.loadMoreRows}
+          class="list"
+          overscan={6}
+        >
+          { items.map(item => (
+            <MovieItem
+              onPressMovie={this.props.onPressMovie}
+              onPressFavorite={this.props.onPressFavorite}
+              item={item}
+            />
+          )) }
+        </RecyclerList>
+        { items && items.length !== totalItems ?
+          <div className={style.progressContainer}>
+            <CircularProgress color="secondary" />
+          </div>
+          :
+          null
+        }
+      </div>
     );
   }
 
