860829a9e55bcba87997134694fa45d0257f57ed
Witalo Benício <contato@witalobenicio.com>
2018-11-28 22:00:21 -0300

    init serviceWork before render

--- src/index.js
+++ src/index.js
@@ -4,5 +4,5 @@ import './index.css';
 import App from './App';
 import serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
 serviceWorker();
+ReactDOM.render(<App />, document.getElementById('root'));
